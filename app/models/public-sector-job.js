e77bd02b7d44063fa7aec4393a49fad0ed7cc65f
JustinNazari <justinnazari@gmail.com>
2016-06-16 15:04:52 -0400

    not working

---
+++ app/models/public-sector-job.js
@@ -0,0 +1,9 @@
+import Model from 'ember-data/model';
+import attr from 'ember-data/attr';
+import { belongsTo, hasMany } from 'ember-data/relationships';
+
+export default Model.extend({
+  grade: attr('number'),
+  min: attr('number'),
+  max: attr('number')
+});
