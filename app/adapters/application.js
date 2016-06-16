ff8de71b3039d90d0a55b5e77652c1a792339055
JustinNazari <justinnazari@gmail.com>
2016-06-16 16:18:48 -0400

    IT WORKS

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -2,5 +2,9 @@ import JSONAPIAdapter from 'ember-data/adapters/json-api';
 
 export default JSONAPIAdapter.extend({
   host: "http://localhost:3000",
-  namespace: "api/v1"
+  namespace: "api/v1",
+  pathForType: function(type) {
+    let path = Ember.String.underscore(type)
+    return Ember.String.pluralize(path);
+  }
 });
