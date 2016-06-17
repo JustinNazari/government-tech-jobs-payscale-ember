5fec1c47538847d335da8678ea7b5edb57c7893b
JustinNazari <justinnazari@gmail.com>
2016-06-17 11:15:55 -0400

    working

--- app/models/section.js
+++ app/models/section.js
@@ -4,5 +4,10 @@ import { belongsTo, hasMany } from 'ember-data/relationships';
 
 export default Model.extend({
   name: attr(),
-  privateSectorJobs: hasMany('private-sector-job')
+  privateSectorJobs: hasMany('private-sector-job'),
+  chartData: Ember.computed(function() {
+    let jobs = this.get('privateSectorJobs')
+    return jobs
+    })
+  })
 });
