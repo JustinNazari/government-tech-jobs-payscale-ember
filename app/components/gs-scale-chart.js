0966e87e35258e4580d51f9049526bfeff2e658f
alemosie <alexandra.siega@gmail.com>
2016-06-18 17:52:46 -0400

    working gs scale chart

--- app/components/gs-scale-chart.js
+++ app/components/gs-scale-chart.js
@@ -1,22 +1,16 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  // gsJobChartData: Ember.computed(function() {
-  //   let data = [];
-  //   let jobs = this.store.findAll('public-sector-job');
-  //   let grade = this.get('grade');
-  //   let min = this.get('min');
-  //   let max = this.get('max')
-  //   let min_hash = {"label": "1: Min", "group": grade, "value": min}
-  //   let max_hash = {"label": "2: Max", "group": grade, "value": max}
-  //   data.push(min_hash);
-  //   data.push(max_hash);
-  //   return data
-  // })
-  //
-  // gsJobsData: Ember.computed(function() {
-  //
-  // }
-
-
+  gsChartData: Ember.computed("model", function() {
+    let model = this.get('model');
+    var chart_data = [];
+    model.content.map(function(job) {
+      let data = job._data;
+      chart_data.pushObjects([
+        { "label": 'Min', "group": data.grade, "value": data.min },
+        { "label": 'Max', "group": data.grade, "value": data.max }
+      ]);
+    });
+    return chart_data;
+  })
 });
--- app/controllers/public-sector-jobs.js
+++ app/controllers/public-sector-jobs.js
@@ -1,7 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  gsChartData: Ember.computed(function() {
+  allJobs: Ember.computed(function() {
     return this.store.findAll('public-sector-job')
-   })
+  })
 });
