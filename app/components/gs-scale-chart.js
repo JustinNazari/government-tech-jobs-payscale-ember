09cab4ec66a07849f979356708a4e5a0e5d1652e
alemosie <alexandra.siega@gmail.com>
2016-06-18 15:55:28 -0400

    component gs-scale-chart talks to pbj

--- app/components/gs-scale-chart.js
+++ app/components/gs-scale-chart.js
@@ -1,14 +1,22 @@
+import Ember from 'ember';
+
 export default Ember.Component.extend({
-  gsChartData: Ember.computed(function() {
-    let jobs = this.store.findAll('public-sector-job');
-    let data = [];
-    let grade = this.get('grade');
-    let min = this.get('min');
-    let max = this.get('max')
-    let min_hash = {"label": "1: Min", "group": grade, "value": min}
-    let max_hash = {"label": "2: Max", "group": grade, "value": max}
-    all_data.push(min_hash);
-    all_data.push(max_hash);
-    return all_data
-  })
+  // gsJobChartData: Ember.computed(function() {
+  //   let data = [];
+  //   let jobs = this.store.findAll('public-sector-job');
+  //   let grade = this.get('grade');
+  //   let min = this.get('min');
+  //   let max = this.get('max')
+  //   let min_hash = {"label": "1: Min", "group": grade, "value": min}
+  //   let max_hash = {"label": "2: Max", "group": grade, "value": max}
+  //   data.push(min_hash);
+  //   data.push(max_hash);
+  //   return data
+  // })
+  //
+  // gsJobsData: Ember.computed(function() {
+  //
+  // }
+
+
 });
--- app/models/public-sector-job.js
+++ app/models/public-sector-job.js
@@ -5,5 +5,6 @@ import { belongsTo, hasMany } from 'ember-data/relationships';
 export default Model.extend({
   grade: attr('number'),
   min: attr('number'),
-  max: attr('number')
+  max: attr('number'),
+
 });
