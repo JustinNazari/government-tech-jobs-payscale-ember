231414939e33f80a347d48dd8988af5a623b7c54
JustinNazari <justinnazari@gmail.com>
2016-06-20 16:52:26 -0400

    working skills chart

---
+++ app/components/section-chart.js
@@ -0,0 +1,15 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+privateSectorJobsChartData: Ember.computed("model", function() {
+  let model = this.get('model')
+  let chart_data = []
+  model.get('privateSectorJobs').forEach(function(item){
+    let min_hash = {"group": item.get('title'), "value": item.get('min'), "label": "Minimum Salary"}
+    let max_hash = {"group": item.get('title'), "value": item.get('max'), "label": "Maximum Salary"}
+    chart_data.push(min_hash)
+    chart_data.push(max_hash)
+  })
+  return chart_data
+})
+});
