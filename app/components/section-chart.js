2d131bd695569cd2bbd114c012a943af49457c07
alemosie <alexandra.siega@gmail.com>
2016-06-21 11:49:29 -0400

    added color to charts

--- app/components/section-chart.js
+++ app/components/section-chart.js
@@ -5,8 +5,8 @@ privateSectorJobsChartData: Ember.computed("model", function() {
   let model = this.get('model')
   let chart_data = []
   model.get('privateSectorJobs').forEach(function(item){
-    let min_hash = {"group": item.get('title'), "value": item.get('min'), "label": "Minimum Salary"}
-    let max_hash = {"group": item.get('title'), "value": item.get('max'), "label": "Maximum Salary"}
+    let min_hash = {"group": item.get('title'), "value": item.get('min'), "label": "Dark: Minimum Salary"}
+    let max_hash = {"group": item.get('title'), "value": item.get('max'), "label": "Light: Maximum Salary"}
     chart_data.push(min_hash)
     chart_data.push(max_hash)
   })
