4af16406f829672a09614f5c8bc617383186dec7
JustinNazari <justinnazari@gmail.com>
2016-06-21 10:35:08 -0400

    states

--- app/components/states-list.js
+++ app/components/states-list.js
@@ -3,10 +3,12 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   states: Ember.computed("model", function() {
     let model = this.get('model');
-    let states = [];
+    let statenames = [];
+    let states = []
     model.forEach(function(item) {
-      if (states.indexOf(item.get('state')) === -1) {
-        states.push(item.get('state'))
+      if (statenames.indexOf(item.get('state')) === -1) {
+        statenames.push(item.get('state'))
+        states.pushObject(item)
       }
     })
     return states;
